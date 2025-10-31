1a020971d3d5f4097a2e010207e8cd85fb42644f
Ailafeka <ailafeka@gmail.com>
2025-10-31 18:48:28 +0800

    fix:修正ESLint錯誤問題

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const apipush = async() =>{
     const res = await fetch('/api/addStore',{
       method:"POST",
     })
+    console.log(res);
   }
 
 
